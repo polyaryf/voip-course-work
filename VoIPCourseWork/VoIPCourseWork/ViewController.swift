8fc779fffbf42b0d2a0883db80f5297d204a18a8
Polina Ryftina <pryftina@gmail.com>
2024-05-27 12:25:19 +0300

    init project with pods

---
+++ VoIPCourseWork/VoIPCourseWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VoIPCourseWork
+//
+//  Created by Полина Рыфтина on 27.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
